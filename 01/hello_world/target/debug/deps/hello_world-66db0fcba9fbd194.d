31fe6d1df706886940d4acde0bcefc86931c6b23
guoyubo <guoyubo@makeblock.com>
2023-06-05 10:06:26 +0800

    feat: init

---
+++ 01/hello_world/target/debug/deps/hello_world-66db0fcba9fbd194.d
@@ -0,0 +1,5 @@
+/Users/guoyubo/Desktop/rust/01/hello_world/target/debug/deps/hello_world-66db0fcba9fbd194: src/main.rs
+
+/Users/guoyubo/Desktop/rust/01/hello_world/target/debug/deps/hello_world-66db0fcba9fbd194.d: src/main.rs
+
+src/main.rs:
---
+++ 01/hello_world/target/debug/hello_world.d
@@ -0,0 +1,1 @@
+/Users/guoyubo/Desktop/rust/01/hello_world/target/debug/hello_world: /Users/guoyubo/Desktop/rust/01/hello_world/src/main.rs
